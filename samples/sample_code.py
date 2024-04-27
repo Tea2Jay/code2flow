ff227ae654732dc7341bd5292d5ee1ad2b99f31a
Tea2Jay <itrq222@gmail.com>
2024-04-27 23:16:58 +0300

    move code samples to `samples`

---
+++ samples/sample_code.py
@@ -0,0 +1,10 @@
+def main():
+    if name == "John" and name == "Paul":
+        print(name)
+    if name == "t":
+        print(name)
+    else:
+        if "x" in "xxxx":
+            print("x")
+        print("Not John or Paul")
+    
